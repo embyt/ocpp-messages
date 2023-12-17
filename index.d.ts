bc286aebac016963f2c6b122252d3dbc34729221
Roman Morawek <roman.morawek@embyt.com>
2023-12-17 23:46:49 +0100

    extract enums and general types

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// general OCPP type definitions
+
+export enum MessageType {
+  CALL = 2,
+  CALLRESULT = 3,
+  CALLERROR = 4,
+}
--- v1.6/MeterValues.d.ts
+++ v1.6/MeterValues.d.ts
@@ -5,6 +5,72 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
+export type ReadingContextEnumType =
+  | "Interruption.Begin"
+  | "Interruption.End"
+  | "Sample.Clock"
+  | "Sample.Periodic"
+  | "Transaction.Begin"
+  | "Transaction.End"
+  | "Trigger"
+  | "Other";
+
+export type MeasurandEnumType =
+  | "Energy.Active.Export.Register"
+  | "Energy.Active.Import.Register"
+  | "Energy.Reactive.Export.Register"
+  | "Energy.Reactive.Import.Register"
+  | "Energy.Active.Export.Interval"
+  | "Energy.Active.Import.Interval"
+  | "Energy.Reactive.Export.Interval"
+  | "Energy.Reactive.Import.Interval"
+  | "Power.Active.Export"
+  | "Power.Active.Import"
+  | "Power.Offered"
+  | "Power.Reactive.Export"
+  | "Power.Reactive.Import"
+  | "Power.Factor"
+  | "Current.Import"
+  | "Current.Export"
+  | "Current.Offered"
+  | "Voltage"
+  | "Frequency"
+  | "Temperature"
+  | "SoC"
+  | "RPM";
+
+export type PhaseEnumType =
+  | "L1"
+  | "L2"
+  | "L3"
+  | "N"
+  | "L1-N"
+  | "L2-N"
+  | "L3-N"
+  | "L1-L2"
+  | "L2-L3"
+  | "L3-L1";
+
+export type LocationEnumType = "Cable" | "EV" | "Inlet" | "Outlet" | "Body";
+
+export type UnitEnumType =
+  | "Wh"
+  | "kWh"
+  | "varh"
+  | "kvarh"
+  | "W"
+  | "kW"
+  | "VA"
+  | "kVA"
+  | "var"
+  | "kvar"
+  | "A"
+  | "V"
+  | "K"
+  | "Celcius"
+  | "Fahrenheit"
+  | "Percent";
+
 export interface MeterValuesRequest {
   connectorId: number;
   transactionId?: number;
@@ -12,58 +78,12 @@ export interface MeterValuesRequest {
     timestamp: string;
     sampledValue: {
       value: string;
-      context?:
-        | "Interruption.Begin"
-        | "Interruption.End"
-        | "Sample.Clock"
-        | "Sample.Periodic"
-        | "Transaction.Begin"
-        | "Transaction.End"
-        | "Trigger"
-        | "Other";
+      context?: ReadingContextEnumType;
       format?: "Raw" | "SignedData";
-      measurand?:
-        | "Energy.Active.Export.Register"
-        | "Energy.Active.Import.Register"
-        | "Energy.Reactive.Export.Register"
-        | "Energy.Reactive.Import.Register"
-        | "Energy.Active.Export.Interval"
-        | "Energy.Active.Import.Interval"
-        | "Energy.Reactive.Export.Interval"
-        | "Energy.Reactive.Import.Interval"
-        | "Power.Active.Export"
-        | "Power.Active.Import"
-        | "Power.Offered"
-        | "Power.Reactive.Export"
-        | "Power.Reactive.Import"
-        | "Power.Factor"
-        | "Current.Import"
-        | "Current.Export"
-        | "Current.Offered"
-        | "Voltage"
-        | "Frequency"
-        | "Temperature"
-        | "SoC"
-        | "RPM";
-      phase?: "L1" | "L2" | "L3" | "N" | "L1-N" | "L2-N" | "L3-N" | "L1-L2" | "L2-L3" | "L3-L1";
-      location?: "Cable" | "EV" | "Inlet" | "Outlet" | "Body";
-      unit?:
-        | "Wh"
-        | "kWh"
-        | "varh"
-        | "kvarh"
-        | "W"
-        | "kW"
-        | "VA"
-        | "kVA"
-        | "var"
-        | "kvar"
-        | "A"
-        | "V"
-        | "K"
-        | "Celcius"
-        | "Fahrenheit"
-        | "Percent";
+      measurand?: MeasurandEnumType;
+      phase?: PhaseEnumType;
+      location?: LocationEnumType;
+      unit?: UnitEnumType;
       [k: string]: unknown;
     }[];
     [k: string]: unknown;
